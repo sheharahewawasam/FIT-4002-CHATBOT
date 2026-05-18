c9597684e1dc106f59558c0d5fd73d70766fba39
Mark <markshim674@gmail.com>
2026-05-18 17:19:04 +1000

    Got the user Ui done and some backend user dicts setup, not fully implemetned yet

---
+++ mvp_demo/rag_api/users.py
@@ -0,0 +1,6 @@
+class pretendDatabase: 
+    userList = {"John" : ["Sample Superannuation Fund", "Summers Family Super Fund", "General"],
+                "Emily" : ["Ausis Super Fund", "General"],
+                "Jake" : ["Triple A Super", "General"],
+                "Shrek" : ["General"]
+                }
