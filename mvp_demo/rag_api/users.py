33bc3909e0e777d9fb943e7dc91362753a4c77aa
Mark <markshim674@gmail.com>
2026-05-24 22:04:59 +1000

    finished on the front end users retrieval

--- mvp_demo/rag_api/users.py
+++ mvp_demo/rag_api/users.py
@@ -1,6 +1,22 @@
-class pretendDatabase: 
-    userList = {"John" : ["Sample Superannuation Fund", "Summers Family Super Fund", "General"],
-                "Emily" : ["Ausis Super Fund", "General"],
-                "Jake" : ["Triple A Super", "General"],
-                "Shrek" : ["General"]
-                }
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+    
+userList = {"John" : ["Sample Superannuation Fund", "Summers Family Super Fund", "General"],
+            "Emily" : ["Ausis Super Fund", "General"],
+            "Jake" : ["Triple A Super", "General"],
+            "Shrek" : ["General"]
+            }
+
+@api_view(['GET'])
+def get_users(request):
+    users = list(userList.keys())
+    return JsonResponse({"users": users})
+
+@api_view(['GET'])
+def get_funds(request, name):
+    data = userList.get(name)
+
+    if data is None:
+        return JsonResponse({"error": "User not found"}, status=404)
+
+    return JsonResponse({"data": data})
